c3ad538ae5c295b85c6237b68bfcfe2d1456aea0
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2021-09-05 20:55:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#count letters  occurence
+    
+def count_characters(s):
+    count = {}
+    for i in s:
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+    print(count)
+print(count_characters("Pooja Patel"))
